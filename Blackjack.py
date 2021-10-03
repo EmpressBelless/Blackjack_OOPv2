2acbd91243d3f7347a1a7a93f89070392b946cd8
EmpressBelless <suzette.wallin@outlook.com>
2021-10-03 16:24:50 -0500

    created working for loop to show cards

--- Blackjack.py
+++ Blackjack.py
@@ -1,6 +1,5 @@
 import random
 
-#https://www.youtube.com/watch?v=C82s5WufNUA
 class Card:
   def __init__(self, suit, value):
     self.suit = ["Hearts", "Spades", "Clubs", "Diamonds"]
@@ -8,69 +7,62 @@ class Card:
 
   def facecard(self):
     if self.value == 'J' or 'Q' or 'K':
-      self.value = 10
+      self.points = 10
 
-  def __repr__(self):
-    return " of ".join((self.value, self.suit))
+  def show(self):
+    print(f'("{self.suit} of {self.value}")')
 
 class Deck:
   def __init__(self):
-      self.cards = []
+    self.cards = []
+    self.shuffle()
+
   def shuffle(self):
-    pass 
-  def draw(self, something):
-    cards = []
-    deck_number = random.choice(Dealer.deck).pop()
-    deck_suit = random.shuffle(Dealer.suit).pop()
+    for i in range(1, 14):
+      for j in ("Spades", "Clubs", "Diamonds", "Hearts"):
+        self.cards.append(Card(j, i))
+        print(f'("{i} of {j}")')
+        print(len(self.cards))
 
-    # for i in Dealer.deck:
-    #   Dealer_hand = random.shuffle(Dealer.deck).pop()
-    # for i in Dealer.suit:
-    #   Dealer_hand = random.shuffle(Dealer.suit).pop()
-    # for i in Dealer.deck:
-    #   Player_hand = random.shuffle(Dealer.deck).pop()
-    # for i in Dealer.suit:
-    #   Player_hand = random.shuffle(Dealer.suit).pop()
-  def hand():
-    pass
-class Player:
-  def __init__(self, Player):
-    self.player = Player
+deck = Deck()
+deck.show()
+#   def draw(self):
+#     self.cards = []
+#     deck_number = random.choice(self.value).pop()
+#     deck_suit = random.choice(self.suit).pop()
+#         # return " of ".join((self.value, self.suit))
+#     print(f'("Player has {deck_number} and {deck_suit} ")')
+# # class Player:
 
-  def hit():
-    pass
-  def stand(self, value):
-    # def discard(self):
-    # how could I discard the deck or should I say we can only play once...
-    pass
+#     for i in Dealer.deck:
+#       Dealer_hand = random.shuffle(Dealer.deck).pop()
+#     for i in Dealer.suit:
+#       Dealer_hand = random.shuffle(Dealer.suit).pop()
+#     for i in Dealer.deck:
+#       Player_hand = random.shuffle(Dealer.deck).pop()
+#     for i in Dealer.suit:
+#     #   Player_hand = random.shuffle(Dealer.suit).pop()
+#   def hand():
+#     pass
 
+#   def hit():
+#     pass
 
-class Game:
-  def run():
-    while True:
-      response = input("Welcome! Let's Play! Enter Play to begin: ")
-      if response.lower == "Play":
-        Deck.shuffle()
-      if response.lower() == "hit":
-        Player.hit()
-      if response.lower() == "stand":
-        Player.stand()
-  run()
+#   def stand(self, value):
+#     # def discard(self):
+#     # how could I discard the deck or should I say we can only play once...
+#     pass
 
-
-
-Player1 = Player("Suzette")
-Player1.run()
-
-#Class Name():
-   #def init(self, last_n, f_n, age)
-   #self. last-n = last_n
-   #sef.first_n = first_n
-  #  #self.age = age
-  # #def say_if_minor(self):
-  #   if self.age < 21:
-  #     print("this patient is a minor")
-
-  #instead of saying def say_if_minor(pid4343.first_n, 4343,last_n, pid4343.age)
-  #you'd write
-  # def say_if_minor(self)
+# class Game:
+#   def run():
+#     while True:
+#       response = input("Welcome! Let's Play! Enter Play to begin: ").lower()
+#       if response == "play":
+#         Deck.shuffle(response)
+#     # if response.lower() == "hit":
+#     #   Player.hit()
+#     # if response.lower() == "stand":
+#     #   Player.stand()
+#       else:
+#         print("That is incorrect.  Please try again.")
+#   run()
