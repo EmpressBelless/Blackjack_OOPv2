eb3fdee8768210593c11d525babf2e583c121f2c
EmpressBelless <suzette.wallin@outlook.com>
2021-10-04 00:01:54 -0500

    Working through my draw steps

--- Blackjack.py
+++ Blackjack.py
@@ -19,6 +19,7 @@ class Deck:
     for i in range(1,14):
       for j in ("Spades", "Hearts", "Diamonds", "Clubs"):
         self.cards.append(Card(i, j))
+    Deck.shuffle(self)
         #print(f'("{i} of {j}")')
 
   def shuffle(self): #shuffles my deck
@@ -26,13 +27,11 @@ class Deck:
 
   def show(self): # if I want to see all of the cards in my deck
     for see in self.cards:
-      Card.show(see)
+      Deck.show(see)
 
   def draw(self):  # I pop(1) card out of my list and put it in single_card. Deck should already be shuffled. 
     single_card = self.cards.pop()
     return single_card
-    # print(f'("Player has {deck_number} and {deck_suit} ")')
-
 
 class Player:
   def __init__(self, name):
@@ -40,7 +39,7 @@ class Player:
     self.playerhand = []
 
   def draw(self): #This is for player. confirmation confirmed how many cards he drew
-    self.playerhand = Deck.cards.pop() * 2
+    playerhand = Deck.cards.pop(self)
     print(f'("Player drew {len(self.playerhand)} cards")')
 
   def showhand(self):
@@ -54,36 +53,39 @@ class Dealer(Player):
       self.aces = 0
       self.dealerhand = []
 
-  def draw(self):  #Dealer draws 2 cards and they're stored in dealer_hand
-    self.dealer = Deck.cards.pop() * 2
-    print(f'("Player drew {len(self.dealer)} cards")')
-
-  def adjust(ace):
+  def draw(self):  #Dealer draws cards and they're stored in dealer_hand
+    self.dealerhand = Deck.cards.pop(self)
+    print(f'("Player drew {len(self.dealerhand)} cards")')
 
 #   def hit():
 #     pass
 
 #   def stand(self, value):
 #     pass
-
+newplayer = Deck()
+newplayer1 = newplayer
 class Game:
+
   def run():
     while True:
-      Deck.build()
-
       response = input("Welcome! Let's Play! Enter Play to begin: ")
       if response.lower() == "play":
-          Deck.build()
-          Deck.shuffle()
+          Deck.build(newplayer1)
           print("Deck has been shuffled!")
-          Dealer.draw()
-          Player.draw()
+          Dealer.draw(newplayer1)
+          Dealer.draw(newplayer1)
+          Player.draw(newplayer1)
+          Player.draw(newplayer1)
           Player.showhand()
-
       elif response.lower() == "hit":
         Player.hit()
+        player.Deck.draw()
+        Dealer.Deck.draw()
       elif response.lower() == "stand":
         Player.stand()
       else:
         print("That is incorrect.  Please try again.")
-    run()
+  run()
+
+  
+
