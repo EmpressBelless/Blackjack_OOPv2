b359adb0b4a32aca9725abcdc693632f37293775
EmpressBelless <suzette.wallin@outlook.com>
2021-10-03 21:44:46 -0500

    Added new classes and added instances in run

--- Blackjack.py
+++ Blackjack.py
@@ -2,61 +2,88 @@ import random
 
 class Card:
   def __init__(self, suit, value):
-    self.suit = ["Hearts", "Spades", "Clubs", "Diamonds"]
-    self.value = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
-
-  def facecard(self, points):
-    if self.value == 'J' or 'Q' or 'K':
-      self.points = 10
+    self.suit = suit
+    # ["Hearts", "Spades", "Clubs", "Diamonds"]
+    self.value = value
+    # ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
 
   def show(self):
     print(f'("{self.suit} of {self.value}")')
 
-amazing = Card()
-amazing.show()
 class Deck:
   def __init__(self):
     self.cards = []
-    self.shuffle()
+    self.build()
 
-  def shuffle(self): #missing one argument
-    for i in range(1, 14):
-      for j in ("Spades", "Clubs", "Diamonds", "Hearts"):
-        self.cards.append(Card(j, i))
-        # print(f'("{i} of {j}")')
+  def build(self): 
+    for i in range(1,14):
+      for j in ("Spades", "Hearts", "Diamonds", "Clubs"):
+        self.cards.append(Card(i, j))
+        #print(f'("{i} of {j}")')
 
-  def draw(self):
-    return self.cards.pop()
+  def shuffle(self): #shuffles my deck
+    random.shuffle(self.cards)
+
+  def show(self): # if I want to see all of the cards in my deck
+    for see in self.cards:
+      Card.show(see)
+
+  def draw(self):  # I pop(1) card out of my list and put it in single_card. Deck should already be shuffled. 
+    single_card = self.cards.pop()
+    return single_card
     # print(f'("Player has {deck_number} and {deck_suit} ")')
+
 
 class Player:
   def __init__(self, name):
     self.name = name
-    self.hand = []
+    self.playerhand = []
 
-  def draw(self):
-    self.hand.append(Deck.draw())
-    return self
+  def draw(self): #This is for player. confirmation confirmed how many cards he drew
+    self.playerhand = Deck.cards.pop() * 2
+    print(f'("Player drew {len(self.playerhand)} cards")')
 
-  def hand(self):
-    for card in self.hand:
-      card.show()
+  def showhand(self):
+    for card in self.playerhand:
+      Card.showhand(card)
 
-  def hit():
-    pass
+class Dealer(Player):
+  def __init__(self, name):
+      super().__init__(name)
+      self.value = 0
+      self.aces = 0
+      self.dealerhand = []
 
-  def stand(self, value):
-    pass
+  def draw(self):  #Dealer draws 2 cards and they're stored in dealer_hand
+    self.dealer = Deck.cards.pop() * 2
+    print(f'("Player drew {len(self.dealer)} cards")')
+
+  def adjust(ace):
+
+#   def hit():
+#     pass
+
+#   def stand(self, value):
+#     pass
 
 class Game:
   def run():
-    response = input("Welcome! Let's Play! Enter Play to begin: ")
-    if response.lower() == "play":
-      Deck.shuffle(self)  #I think this expects an attribute
-    elif response.lower() == "hit":
-      Player.hit()
-    elif response.lower() == "stand":
-      Player.stand()
-    else:
-      print("That is incorrect.  Please try again.")
-  run()
+    while True:
+      Deck.build()
+
+      response = input("Welcome! Let's Play! Enter Play to begin: ")
+      if response.lower() == "play":
+          Deck.build()
+          Deck.shuffle()
+          print("Deck has been shuffled!")
+          Dealer.draw()
+          Player.draw()
+          Player.showhand()
+
+      elif response.lower() == "hit":
+        Player.hit()
+      elif response.lower() == "stand":
+        Player.stand()
+      else:
+        print("That is incorrect.  Please try again.")
+    run()
